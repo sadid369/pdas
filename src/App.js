ad8b003b5c2e06fd60c6aa237c0a5f784937453d
Sadid <sadid.jones@gmail.com>
2023-01-29 22:01:26 +0600

    @29/01/2023

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import ProfileCard from "./profileCard";
+import "bulma/css/bulma.css";
+import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
 function App(params) {
   return (
     <div>
-      <div>Personal Digital Assistants</div>
-
-      <ProfileCard />
-      <ProfileCard />
-      <ProfileCard />
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
+      {/* <div>Personal Digital Assistants</div> */}
+      <div className="container">
+        <div className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                image={AlexaImage}
+                description="Dolore eiusmod incididunt
+                 eu dolor id culpa ut et consectetur adipisicing."
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana32"
+                image={CortanaImage}
+                description="Dolore eiusmod incididunt
+                 eu dolor id culpa ut et consectetur adipisicing."
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Siri"
+                handle="@siri01"
+                image={SiriImage}
+                description="Dolore eiusmod incididunt
+                 eu dolor id culpa ut et consectetur adipisicing."
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/profileCard.js
+++ src/profileCard.js
@@ -1,7 +1,27 @@
 import React from "react";
-
-function ProfileCard() {
-  return <div>Profile Card !!!</div>;
+// import AlexaImage from "./images/alexa.png";
+// import CortanaImage from "./images/cortana.png";
+// import SiriImage from "./images/siri.png";
+// console.log(AlexaImage);
+// console.log(SiriImage);
+function ProfileCard({ title, handle, image, description }) {
+  // const { title, handle } = props;
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt="pda logo" />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">{title}</p>
+          <p className="subtitle is-6"> {handle}</p>
+        </div>
+        <div className="content">{description}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProfileCard;
